6d9ed37232c22ec6a34579686a5da9ae5539a969
GenialX <admin@ihuxu.com>
2017-08-08 20:40:23 +0800

    optimization

--- 2017/08/longest-palindromic-substring.cpp
+++ 2017/08/longest-palindromic-substring.cpp
@@ -1,8 +1,7 @@
 /**
  * https://leetcode.com/problems/longest-palindromic-substring/solution/
  *
- * this demo is very terrible.
- * dp time O(n^2) space O(n^2)
+ * this demo:using the dp method, time O(n^2) space O(n^2)
  *
  */
 #include <iostream>
@@ -10,21 +9,22 @@
 #include <map>
 #include <string>
 
-#define MAX 1000
+#define MAX 1001
 
 using namespace std;
 
-int max_l, offset;
-int dp[MAX][MAX];
-
 class Solution {
-    public:
-        string longestPalindrome(string s) {
-            int l = s.length();
+    private:
+        string s;
 
+    public:
+        string longestPalindrome(string ori_s) {
+            s = ori_s;
+            int dp[MAX][MAX] = {0};
+            int max_l = 0, offset = 0, l = s.length();
             for (int i = 0; i < l; ++i) {
                 for (int j = i; j < l; ++j) {
-                    if (isPalindrome(s.substr(i, j - i + 1), i, j - i + 1)) {
+                    if (isPalindrome(i, j, dp)) {
                         if (j - i + 1> max_l) {
                             offset = i;
                             max_l = j - i + 1;
@@ -36,38 +36,24 @@ class Solution {
         }
 
     private :
-        bool isPalindrome(string s, int offset, int length) {
-            if (dp[offset][offset + length - 1] < 0) {
-                return dp[offset][offset + length - 1] == -1 ? true : false;
+        bool isPalindrome(int start, int end, int dp[MAX][MAX]) {
+            if (dp[start][end] == 0) {
+                switch (end - start + 1) {
+                    case 1:
+                        dp[start][end] = -1;
+                        break;
+                    case 2:
+                        dp[start][end] = s[start] == s[end] ? -1 : -2;
+                        break;
+                    default:
+                        dp[start][end] = (s[start] == s[end] && isPalindrome(start + 1, end - 1, dp))? -1 : -2;
+                }
             }
-            int l = s.length();
-            switch (l) {
-                case 1:
-                    dp[offset][offset + length - 1] = -1;
-                    return true;
-                    break;
-                case 2:
-                    if (s[0] == s[1]) {
-                        dp[offset][offset + length - 1] = -1;
-                        return true;
-                    } else {
-                        dp[offset][offset + length - 1] = -2;
-                        return false;
-                    }
-                    break;
-                default:
-                    if (s[0] == s[l - 1] && isPalindrome(s.substr(1, l - 2), offset + 1, l - 2)) {
-                        dp[offset][offset + length - 1] = -1;
-                        return true;
-                    } else {
-                        dp[offset][offset + length - 1] = -2; 
-                        return false;
-                    }
-            }
+            return dp[start][end] == -1 ? true : false;
         }
 };
 
 int main(int argc, char * argv[]) {
     Solution * s = new Solution();
-    cout<<s->longestPalindrome("babad");
+    cout<<s->longestPalindrome("vommleztyrbrnoij");
 }
