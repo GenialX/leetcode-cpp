86952bbe0163fa48a7dbebc94228c06a303f6ed9
GenialX <admin@ihuxu.com>
2017-08-08 20:51:21 +0800

    cs

--- 2017/08/longest-palindromic-substring.cpp
+++ 2017/08/longest-palindromic-substring.cpp
@@ -18,38 +18,38 @@ class Solution {
         string s;
 
     public:
-        string longestPalindrome(string ori_s) {
-            s = ori_s;
+        string longestPalindrome(string os) {
+            s = os; // os:original string
             int dp[MAX][MAX] = {0};
-            int max_l = 0, offset = 0, l = s.length();
+            int ml = 0, os = 0, l = s.length(); // ml:max length os:offset
             for (int i = 0; i < l; ++i) {
                 for (int j = i; j < l; ++j) {
                     if (isPalindrome(i, j, dp)) {
-                        if (j - i + 1> max_l) {
-                            offset = i;
-                            max_l = j - i + 1;
+                        if (j - i + 1> ml) {
+                            os = i;
+                            ml = j - i + 1;
                         }
                     }
                 }
             }
-            return s.substr(offset, max_l);
+            return s.substr(os, ml);
         }
 
     private :
-        bool isPalindrome(int start, int end, int dp[MAX][MAX]) {
-            if (dp[start][end] == 0) {
-                switch (end - start + 1) {
+        bool isPalindrome(int s, int e, int dp[MAX][MAX]) { // s:start e:end
+            if (dp[s][e] == 0) {
+                switch (e - s + 1) {
                     case 1:
-                        dp[start][end] = -1;
+                        dp[s][e] = -1;
                         break;
                     case 2:
-                        dp[start][end] = s[start] == s[end] ? -1 : -2;
+                        dp[s][e] = s[s] == s[e] ? -1 : -2;
                         break;
                     default:
-                        dp[start][end] = (s[start] == s[end] && isPalindrome(start + 1, end - 1, dp))? -1 : -2;
+                        dp[s][e] = (s[s] == s[e] && isPalindrome(s + 1, e - 1, dp))? -1 : -2;
                 }
             }
-            return dp[start][end] == -1 ? true : false;
+            return dp[s][e] == -1 ? true : false;
         }
 };
 
