b74004df0fcb4f109365f352a4b4e769e584bbb1
admin <admin@ihuxu.com>
2020-02-15 15:56:20 +0800

    :wrench: add code

--- 2020/02/786-k-th-smallest-prime-fraction.cpp
+++ 2020/02/786-k-th-smallest-prime-fraction.cpp
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/k-th-smallest-prime-fraction/
 
+// Binary Search
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+        double l = 0, r = 1, m = 0;
+        int p = 0, q = 1, cnt = 0, n = A.size();;
+        while (true) {
+            m = l + (r - l) / 2.0;
+            cnt = 0, p = 0;
+            for (int i = 0, j = 0; i < n; ++i) {
+                while (j < n && A[i] > m * A[j]) ++j;
+                if (j < n && p * A[j] < q * A[i])
+                    p = A[i], q = A[j];
+                cnt += n - j;
+            }
+            if (cnt == K) return {p, q};
+            if (cnt < K) l = m;
+            else r = m;
+        }
+    }
+};
 
 // Priority Queue
 class Solution {
--- README.md
+++ README.md
@@ -20,6 +20,9 @@ C++ Solutions for LeetCode
 ## Priority Queue
 - [786. K-th Smallest Prime Fraction](https://github.com/huxiaoxu2019/leetcode-cpp/blob/master/2020/02/786-k-th-smallest-prime-fraction.cpp)
 
+## Binary Search
+- [786. K-th Smallest Prime Fraction](https://github.com/huxiaoxu2019/leetcode-cpp/blob/master/2020/02/786-k-th-smallest-prime-fraction.cpp)
+
 # Problems
 
 - [786. K-th Smallest Prime Fraction](https://github.com/huxiaoxu2019/leetcode-cpp/blob/master/2020/02/786-k-th-smallest-prime-fraction.cpp)
